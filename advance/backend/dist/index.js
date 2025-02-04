5395873bf8fb54738beddef0956f2028b080598d
vansh kabra <vanshkabra05@gmail.com>
2025-02-04 23:02:07 +0530

    little update

--- advance/backend/dist/index.js
+++ advance/backend/dist/index.js
@@ -1,94 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = require("ws");
-const server = new ws_1.WebSocketServer({ port: 3000 });
-const clients = new Map();
-const meetings = new Map();
-server.on("connection", (socket) => {
-    console.log("WebSocket server running on ws://localhost:3000");
-    const clientId = generateUniqueId();
-    clients.set(clientId, socket);
-    socket.on("message", (data) => {
-        const message = JSON.parse(data);
-        const { type } = message;
-        switch (type) {
-            case "create-meeting":
-                createMeeting(message.code, clientId);
-                break;
-            case "join-meeting":
-                joinMeeting(message.code, clientId);
-                break;
-            case "signal":
-                handleSignal(message, clientId);
-                break;
-            case "ice-candidate":
-                handleIce(message, clientId);
-                break;
-            case "leave-meeting":
-                leaveMeeting(clientId);
-                break;
-            default:
-                console.error(`Unknown message type: ${type}`);
+const socket_io_1 = require("socket.io");
+const io = new socket_io_1.Server(3000);
+const room = new Map();
+io.on('connection', (socket) => {
+    console.log("user connected", socket.id);
+    socket.on('join-meeting', (code, userid) => {
+        var _a, _b;
+        if (!room.has(code)) {
+            room.set(code, new Set());
         }
-    });
-    socket.on("close", () => {
-        leaveMeeting(clientId);
-        clients.delete(clientId);
+        (_a = room.get(code)) === null || _a === void 0 ? void 0 : _a.add(socket.id);
+        socket.join(code);
+        (_b = socket.to(code)) === null || _b === void 0 ? void 0 : _b.emit('user-joined', { userid });
     });
 });
-function createMeeting(code, clientId) {
-    if (!meetings.has(code)) {
-        meetings.set(code, new Set());
-        console.log(`Meeting ${code} created by ${clientId}.`);
-    }
-    joinMeeting(code, clientId);
-}
-function joinMeeting(code, clientId) {
-    const meeting = meetings.get(code);
-    if (meeting) {
-        meeting.add(clientId);
-        broadcastToMeeting(code, { type: "new-participant", clientId }, clientId);
-        console.log(`Client ${clientId} joined meeting ${code}.`);
-    }
-    else {
-        console.error(`Meeting ${code} does not exist.`);
-    }
-}
-function handleSignal(message, senderId) {
-    const { code, data } = message;
-    broadcastToMeeting(code, { type: "signal", senderId, data }, senderId);
-}
-function handleIce(message, senderId) {
-    const { code, candidate } = message;
-    broadcastToMeeting(code, { type: "ice-candidate", senderId, candidate }, senderId);
-}
-function leaveMeeting(clientId) {
-    for (const [code, meeting] of meetings) {
-        if (meeting.has(clientId)) {
-            meeting.delete(clientId);
-            broadcastToMeeting(code, { type: "participant-left", clientId });
-            console.log(`Client ${clientId} has left.`);
-            if (meeting.size === 0) {
-                meetings.delete(code);
-                console.log(`Meeting ${code} closed.`);
-            }
-            break;
-        }
-    }
-}
-function broadcastToMeeting(code, message, senderId) {
-    const meeting = meetings.get(code);
-    if (meeting) {
-        for (const clientId of meeting) {
-            if (clientId !== senderId) {
-                const clientSocket = clients.get(clientId);
-                if (clientSocket) {
-                    clientSocket.send(JSON.stringify(message));
-                }
-            }
-        }
-    }
-}
-function generateUniqueId() {
-    return Math.random().toString(36).substr(2, 9);
-}
--- advance/backend/src/index.ts
+++ advance/backend/src/index.ts
@@ -1,113 +1,18 @@
-import { WebSocketServer } from "ws";
 
-const server = new WebSocketServer({ port: 3000 });
-const clients = new Map();
-const meetings = new Map();
+import {Server} from "socket.io"
+const io = new Server(3000);
+const room = new Map();
 
-server.on("connection", (socket) => {
-  console.log("WebSocket server running on ws://localhost:3000");
-  const clientId = generateUniqueId();
-  clients.set(clientId, socket);
-
-  socket.on("message", (data: any) => {
-    const message = JSON.parse(data);
-    const { type } = message;
-
-    switch (type) {
-      case "create-meeting":
-        createMeeting(message.code, clientId);
-        break;
-      case "join-meeting":
-        joinMeeting(message.code, clientId);
-        break;
-      case "signal":
-        handleSignal(message, clientId);
-        break;
-      case "ice-candidate":
-        handleIce(message, clientId);
-        break;
-      case "leave-meeting":
-        leaveMeeting(clientId);
-        break;
-      default:
-        console.error(`Unknown message type: ${type}`);
+io.on('connection',(socket)=>{
+  console.log("user connected" , socket.id)
+  socket.on('join-meeting',(code: string , userid)=>{
+    if(!room.has(code)){
+      room.set(code, new Set());
     }
-  });
-
-  socket.on("close", () => {
-    leaveMeeting(clientId);
-    clients.delete(clientId);
-  });
-});
-
-function createMeeting(code: string, clientId: string) {
-  if (!meetings.has(code)) {
-    meetings.set(code, new Set());
-    console.log(`Meeting ${code} created by ${clientId}.`);
-  }
-  joinMeeting(code, clientId);
-}
-
-function joinMeeting(code: string, clientId: string) {
-  const meeting = meetings.get(code);
-  if (meeting) {
-    meeting.add(clientId);
-    broadcastToMeeting(code, { type: "new-participant", clientId }, clientId);
-    console.log(`Client ${clientId} joined meeting ${code}.`);
-  } else {
-    console.error(`Meeting ${code} does not exist.`);
-  }
-}
-
-function handleSignal(message: { code: string; data: any }, senderId: string) {
-  const { code, data } = message;
-  broadcastToMeeting(code, { type: "signal", senderId, data }, senderId);
-}
-
-function handleIce(message: { code: string; candidate: any }, senderId: string) {
-  const { code, candidate } = message;
-  broadcastToMeeting(
-    code,
-    { type: "ice-candidate", senderId, candidate },
-    senderId
-  );
-}
-
-function leaveMeeting(clientId: string) {
-  for (const [code, meeting] of meetings) {
-    if (meeting.has(clientId)) {
-      meeting.delete(clientId);
-      broadcastToMeeting(code, { type: "participant-left", clientId });
-
-      console.log(`Client ${clientId} has left.`);
-
-      if (meeting.size === 0) {
-        meetings.delete(code);
-        console.log(`Meeting ${code} closed.`);
-      }
-      break;
-    }
-  }
-}
-
-function broadcastToMeeting(
-  code: string,
-  message: { type: string; [key: string]: any },
-  senderId?: string
-) {
-  const meeting = meetings.get(code);
-  if (meeting) {
-    for (const clientId of meeting) {
-      if (clientId !== senderId) {
-        const clientSocket = clients.get(clientId);
-        if (clientSocket) {
-          clientSocket.send(JSON.stringify(message));
-        }
-      }
-    }
-  }
-}
-
-function generateUniqueId() {
-  return Math.random().toString(36).substr(2, 9);
-}
+    room.get(code)?.add(socket.id);
+    socket.join(code);
+    socket.to(code)?.emit('user-joined' , {userid})
+  })
+  
+  
+})
